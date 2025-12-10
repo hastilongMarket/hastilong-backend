901a2f244d002d08adf0015d468f720f8f82ad60
Hasti Long <hastilong@gmail.com>
2025-12-10 19:24:15 +0530

    Final Admin Serve Bypass: Set adminCors='' to stop server from looking for missing Admin UI files.

--- medusa-config.js
+++ medusa-config.js
@@ -3,7 +3,7 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 const plugins = [
-  // THE FIX: The incompatible V1 Admin Plugin entry is removed here.
+ 
 ]
 
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS || "http://localhost:8000",
-      adminCors: process.env.ADMIN_CORS || "http://localhost:9000",
+      adminCors: "" ,// process.env.ADMIN_CORS || "http://localhost:9000",
       authCors: process.env.AUTH_CORS || "http://localhost:9000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
