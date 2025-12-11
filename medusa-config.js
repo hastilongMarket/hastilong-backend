c16474155d96ef2d535c476296d8afb27840bf0f
Hasti Long <hastilong@gmail.com>
2025-12-11 19:37:34 +0530

    Setup Medusa backend with Neon DB, Redis pending

--- medusa-config.js
+++ medusa-config.js
@@ -1,21 +1,30 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
-
-const plugins = [
- 
-]
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
+console.log("REDIS_URL:", process.env.REDIS_URL);
 
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redis_url: process.env.REDIS_URL,     
+    eventBus: "redis",    
+    cacheService: "redis",
+    jwtSecret: process.env.JWT_SECRET || "supersecret",
+    cookieSecret: process.env.COOKIE_SECRET || "supersecretcookie",
+
+    server: {
+      port: process.env.PORT ? parseInt(process.env.PORT) : 9000,
+      host: "0.0.0.0",
+    },
+
     http: {
-      storeCors: process.env.STORE_CORS || "http://localhost:8000",
-      adminCors: "" ,// process.env.ADMIN_CORS || "http://localhost:9000",
-      authCors: process.env.AUTH_CORS || "http://localhost:9000",
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+    },
+
+    
   },
-  plugins, // This now passes an empty array, solving the conflict.
-})
+
+  plugins: [],
+});
